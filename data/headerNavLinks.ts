245dca2b9da36e90ba5862528ed74ce75c172513
Slamet <slamet@pon.co.id>
2023-11-21 19:16:37 +0700

    update blogs

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,5 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/ai-chat', title: 'AI Chat' },
   { href: '/activities', title: 'Activities' },
   { href: '/blog', title: 'Blog' },
   { href: '/projects', title: 'Projects' },
