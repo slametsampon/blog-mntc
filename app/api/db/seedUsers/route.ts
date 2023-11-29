88d3f15296bba425c0614aa36bce358856801119
Slamet <slamet@pon.co.id>
2023-11-29 07:46:37 +0700

    test api/seedUsers-route blank

---
+++ app/api/db/seedUsers/route.ts
@@ -0,0 +1,11 @@
+import UserModel from 'models/User'
+import db from 'utils/db'
+import metricsStatic from '@/data/metricsStatic'
+
+export async function GET(request: Request) {
+  // await db.connect()
+  // await UserModel.deleteMany()
+  // await UserModel.insertMany(metricsStatic.users)
+  // await db.disconnect()
+  // return new Response('users seeded successfully')
+}
