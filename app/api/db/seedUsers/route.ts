ff60122adc4fc681cb311c669e584351f4ddd5e7
Slamet <slamet@pon.co.id>
2023-11-29 07:18:00 +0700

    fix User => UserModel

--- app/api/db/seedUsers/route.ts
+++ app/api/db/seedUsers/route.ts
@@ -1,11 +1,11 @@
-import User from 'models/User'
+import UserModel from 'models/User'
 import db from 'utils/db'
 import metricsStatic from '@/data/metricsStatic'
 
 export async function GET(request: Request) {
   await db.connect()
-  await User.deleteMany()
-  await User.insertMany(metricsStatic.users)
+  await UserModel.deleteMany()
+  await UserModel.insertMany(metricsStatic.users)
   await db.disconnect()
   return new Response('users seeded successfully')
 }
