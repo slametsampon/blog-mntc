1b11f007a3f35df100f802541ff9ac2d62579c78
Slamet <slamet@pon.co.id>
2023-12-01 13:47:11 +0700

    CardReliablility:bg-slate-50

--- components/CardReliability.tsx
+++ components/CardReliability.tsx
@@ -12,7 +12,7 @@ export default function CardReliability({ title, reliabilityData }) {
 
   return (
     <>
-      <div className="mt-3 mb-3 rounded-xl bg-green-50  px-5 py-3 shadow-md dark:bg-gray-900">
+      <div className="mt-3 mb-3 rounded-xl bg-slate-50  px-5 py-3 shadow-md dark:bg-gray-900">
         <div className="text-xl mb-3 text-blue-600 dark:text-gray-100 font-semibold">{title}</div>
         {/* Current year data */}
         <div className="bg-blue-50 p-3 rounded-2xl mb-3">
