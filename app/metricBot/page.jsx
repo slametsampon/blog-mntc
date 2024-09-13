77b2990e2e7e742d557785a9caba08f2ecb3f6df
Slamet <slamet@pon.co.id>
2024-09-13 14:19:24 +0700

    Add MetricBot Menu

---
+++ app/metricBot/page.jsx
@@ -0,0 +1,13 @@
+import PageTitle from '@/components/PageTitle'
+export default function Page() {
+  return (
+    <div className="mt-24 text-center">
+      <PageTitle>
+        Under Construction{' '}
+        <span role="img" aria-label="roadwork sign">
+          🚧
+        </span>
+      </PageTitle>
+    </div>
+  )
+}
--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/metrics', title: 'Metrics' },
+  { href: '/metricBot', title: 'MetricBot' },
   { href: '/blog', title: 'Blog' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
