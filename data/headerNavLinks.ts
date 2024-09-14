e6ca380e2a9b1a67816685bfcd8732c52a1b04bd
slametsampon <slamet@pon.co.id>
2024-09-14 15:20:44 +0700

    hide projects

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,10 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
+  //{ href: '/metric', title: 'Metric' },
   { href: '/metricBot', title: 'MetricBot' },
   { href: '/metricSmart', title: 'Metric Smart' },
   { href: '/blog', title: 'Blog' },
-  { href: '/projects', title: 'Projects' },
+  //{ href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
 ]
 
